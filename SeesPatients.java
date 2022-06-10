14c0605c3560ef755512502d084bb913a52b17fd
bushrak3173 <85813974+bushrak3173@users.noreply.github.com>
2022-06-10 02:21:04 -0400

    project2

---
+++ SeesPatients.java
@@ -0,0 +1,13 @@
+
+public interface SeesPatients {
+	
+	static final int MAX_PATIENTS = 100;
+	
+	void addPatient(Patient p) throws PatientException;
+	
+	Patient [] getPatients();
+	
+	String getPatientsAsString();
+	
+	boolean isPatient(Patient p);
+}
